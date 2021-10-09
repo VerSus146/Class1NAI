339f35409ae928d3a7ede08ee8c8607e4e7668c1
Krzysztof Windorpski <s18562@pjwstk.edu.pl>
2021-10-09 13:43:19 +0200

    WIP - rozmieszczenie pionkow

--- main.py
+++ main.py
@@ -18,19 +18,36 @@ class GameOfBones(TwoPlayerGame):
 
     def __init__(self, players=None):
         self.players = players
-        self.board = [0 for i in range(9)]
+        self.board = {
+            {2, 2, 2},
+            {0, 0, 0},
+            {1, 1, 1}
+        }
         self.current_player = 1  # Start with player 1
 
-    def possible_moves(self): return ['1', '2', '3']  # get possible moves
-    def make_move(self, move): self.pile -= int(move)  # remove bones from pile
+    def possible_moves(self): return ['0.0 1.0', '0.1 1.1', '0.2 1.2']  # get possible moves
+
+    def make_move(self, move: [tuple, tuple]):
+        pawn = move[0]
+        move = move[1]
+        if board[move[0]][move[1]] == 0:
+            board[move[0]][move[1]] = curent_player
+
+
     def win(self): return self.pile <= 0  # opponent took the last bone ?
+
     def is_over(self): return self.win()  # game stops when someone wins
+
     def show(self): print("%d bones left in this pile" % self.pile)  # print how many bones are left
+
     def scoring(self): return 100 if game.win() else 0  # Scoring for the AI
 
+        # https://code-with-me.jetbrains.com/mTJ4fRPanxNwyU7Gm1bByw#p=PY&fp=B195FA4AB24EF905E0F154F70B5CD454CBF6279FD76B0E040DEBE20609205BCF
+        # Setup AI and start match
+        ai = Negamax(13)  # AI will think 13 moves in advance
+        game = GameOfBones([Human_Player(), AI_Player(ai)])
+        history = game.play()
 
-# https://code-with-me.jetbrains.com/mTJ4fRPanxNwyU7Gm1bByw#p=PY&fp=B195FA4AB24EF905E0F154F70B5CD454CBF6279FD76B0E040DEBE20609205BCF
-# Setup AI and start match
-ai = Negamax(13)  # AI will think 13 moves in advance
-game = GameOfBones([Human_Player(), AI_Player(ai)])
-history = game.play()
+    class Pawn:
+
+
