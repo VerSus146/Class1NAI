d7e6b7a691471704eca708c11c62668ea816ae96
Winithepooh <imbatman@wp.pl>
2021-11-15 21:49:57 +0100

    master - euclidean algo execution methods

---
+++ Lab3/main.py
@@ -0,0 +1,31 @@
+import parser
+import argparse
+import json
+import numpy as np
+from scoring_algorithms import euclidean_distance
+
+parser.test()
+
+def build_arg_parser():
+    parser = argparse.ArgumentParser(description='Find users who are similar to the input user')
+    parser.add_argument('--user', dest='user', required=True,
+            help='Input user')
+    return parser
+
+# Finds users in the dataset that are similar to the input user
+def find_similar_users(dataset, user, num_users):
+    if user not in dataset:
+        raise TypeError('Cannot find ' + user + ' in the dataset')
+
+    # Compute Pearson score between input user
+    # and all the users in the dataset
+    scores = np.array([[x, euclidean_distance(dataset, user,
+            x)] for x in dataset if x != user])
+
+    # Sort the scores in decreasing order
+    scores_sorted = np.argsort(scores[:, 1])[::-1]
+
+    # Extract the top 'num_users' scores
+    top_users = scores_sorted[:num_users]
+
+    return scores[top_users]
