47598ce46ceb234aafa5606e4805638a22ae64b3
Winithepooh <imbatman@wp.pl>
2021-12-17 21:36:08 +0100

    lab5 - animals recognition done

--- Lab5/Animals/animalRecognition.py
+++ Lab5/Animals/animalRecognition.py
@@ -1,35 +1,52 @@
-#loading the saved model
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
 
 model = load_model('cifar10_model.h5')
-test_image1 = image.load_img('dog2.png')
+bird = image.load_img('animals_images/bird.png')
+dog1 = image.load_img('animals_images/dog.jpg')
+dog2 = image.load_img('animals_images/dog2.png')
+deer = image.load_img('animals_images/deer.png')
 
-test_image =image.img_to_array(test_image1)
-test_image = np.expand_dims(test_image, axis =0)
-result = model.predict(test_image)
-print(result)
-if result[0][0]==1:
-    print("Aeroplane")
-elif result[0][1]==1:
-    print('Automobile')
-elif result[0][2]==1:
-    print('Bird')
-elif result[0][3]==1:
-    print('Cat')
-elif result[0][4]==1:
-    print('Deer')
-elif result[0][5]==1:
-    print('Dog')
-elif result[0][6]==1:
-    print('Frog')
-elif result[0][7]==1:
-    print('Horse')
-elif result[0][8]==1:
-    print('Ship')
-elif result[0][9]==1:
-    print('Truck')
-else:
-    print(result)
-    print('Error')
+counter = 0
+for image_item in [bird, dog1, dog2, deer]:
+
+    # get first image to test
+    test_image = image.img_to_array(image_item)
+
+    #convert to array
+    test_image = np.expand_dims(test_image, axis=0)
+
+    #predict what is this image
+    result = model.predict(test_image)
+
+    #fetch highest possible value out of 10 types - most probable label
+    result = np.argmax(result[0])
+
+    animal_type = 'None'
+    counter += 1
+
+    if result == 0:
+        animal_type = "Aeroplane"
+    elif result == 1:
+        animal_type = 'Automobile'
+    elif result == 2:
+        animal_type = 'Bird'
+    elif result == 3:
+        animal_type = 'Cat'
+    elif result == 4:
+        animal_type = 'Deer'
+    elif result == 5:
+        animal_type = 'Dog'
+    elif result == 6:
+        animal_type = 'Frog'
+    elif result == 7:
+        animal_type = 'Horse'
+    elif result == 8:
+        animal_type = 'Ship'
+    elif result == 9:
+        animal_type = 'Truck'
+    else:
+        animal_type = 'Error'
+
+    print('Image: ' + str(counter) + ' is a ' + animal_type)
