23a6115fcdc7ff21e576455d6b94ff125210d1e4
Krystian <versus146pl@gmail.com>
2021-12-17 11:25:36 +0100

    Lab 5 heartfailure detection - Commenting and cleaning up

--- Lab5/main.py
+++ Lab5/main.py
@@ -3,17 +3,17 @@ Autorzy: Krystian Dąbrowski s18550, Krzysztof Windorpski s18562
 
 Projekt: Systemy klasyfikacji chorob serca, zwierzat, oraz damskiej garderoby z wykorzystaniem sieci neuronowych
 
-Problem: 
+Problem:
 1. Na podstawie danych historycznych przewidujemy jaką wypłatę w tysiącach szwedzkich koron należy wypłacić za daną ilość zgłoszeń
-2. Na podstawie danych 918 pacjentów przewidujemy czy u danej osoby na podstawie jej danych zdrowotnych (tetno, glukoza, itp) 
-   jest ryzyko wystapienia choroby serca 
+2. Na podstawie danych 918 pacjentów przewidujemy czy u danej osoby na podstawie jej danych zdrowotnych (tetno, glukoza, itp)
+   jest ryzyko wystapienia choroby serca
 
 Podsumowanie:
-AutoInsurance - wykonany metodą SLR simple linear regression - wykorzystujący regresję liniową wyznaczającą średnią przewidywaną 
-na podstawie historycznych danych 
+AutoInsurance - wykonany metodą SLR simple linear regression - wykorzystujący regresję liniową wyznaczającą średnią przewidywaną
+na podstawie historycznych danych
 
-HeartFailure - wykonany metodą RBF radial basis function. Ilośc punktów w danym miejscu określa, tzw. "hot spot'y", 
-w które jeżeli wpasują się nasze dane to mamy dużą szansę na poprawny przewidywany wynik. 
+HeartFailure - wykonany metodą RBF radial basis function. Ilośc punktów w danym miejscu określa, tzw. "hot spot'y",
+w które jeżeli wpasują się nasze dane to mamy dużą szansę na poprawny przewidywany wynik.
 
 Patrz: GammaExplanation.png
 
