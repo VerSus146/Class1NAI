89b1c66986b5df842b266ca2a677a3b65a1ef6d2
Winithepooh <imbatman@wp.pl>
2022-01-06 22:41:55 +0100

    Lab6 - playing video, detecting face and eyes, playing warning if we dont watch the commercial ( no eyes detected )

---
+++ Lab6/video_player.py
@@ -0,0 +1,36 @@
+import cv2
+import time
+
+
+class VideoCommercialPlayer:
+    is_paused = True
+    is_video_opened = False
+    video_stream = None
+
+    def __init__(self):
+        self.is_video_opened = self.__openVideoFile()
+        self.is_paused = True
+
+    def __openVideoFile(self):
+        self.video_stream = cv2.VideoCapture('commercials.mp4')
+
+        # check if the video capture is open
+        if not self.video_stream.isOpened():
+            print("Error Opening Video Stream Or File")
+            return False
+        else:
+            return True
+    def displayVideo(self):
+        print ('is_opened: ' + str(self.is_video_opened) + ' is_paused:' + str(self.is_paused))
+        if self.is_video_opened and not self.is_paused:
+            ret, frame = self.video_stream.read()
+
+            if ret == True:
+                cv2.resizeWindow('frame', 600, 600)
+                cv2.imshow('frame', frame)
+
+    def pauseVideo(self):
+        self.is_paused = True
+
+    def playVideo(self):
+        self.is_paused = False
